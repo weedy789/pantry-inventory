cf4c6191d4a2b6d90f5717615cf19717f56dd80a
weedy798 <brandon@QBIT012.ksbau2.local>
2022-12-12 12:11:49 +1000

    boilerplate code

--- pantry-organiser/src/App.js
+++ pantry-organiser/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom;
+import { React } from 'react';
+
+//components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+//pages 
+import "./pages/App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
