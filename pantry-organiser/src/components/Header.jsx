288294ef1d0b6a34eda354a7cb16793fca767dd9
weedy798 <wiedman11@gmail.com>
2022-12-13 14:57:26 +1000

    ag grid boilerplate

--- pantry-organiser/src/components/Header.jsx
+++ pantry-organiser/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Header(
+export default function Header() {
   return (
     <header> 
       <h4> <b> Pantry Organiser </b> </h4>
--- pantry-organiser/src/pages/Home.jsx
+++ pantry-organiser/src/pages/Home.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 //import "./App.css";
+import { AgGridReact } from 'ag-grid-react'; 
+import 'ag-grid-community/styles/ag-grid.css'; //core grid CSS for AgGrid
+import 'ag-grid-community/styles/ag-theme-alpine.css'; //optional css for AgGrid
+
+const [rowData, setRowData] = useState(); //set rowData to array of objects
+const [columnDefs, setColumnDefs] = useState([
+  {field: 'make', filter: true}, 
+  {field: 'model', filter:true},
+  {field: 'price'}
+]);
+
+//example data from AgGrid server
+useEffect(() => {
+  fetch('https://www.ag-grid.com/example-assets/row-data.json')
+  .then(result => result.json())
+  .then(rowData => setRowData(rowData))
+}, []);
+
 
 export default function Home() {
   return (
     <div>
       <Container>
         <centre>
-          <h1> boobs </h1>
+          <h1> hi </h1>
+          <AgGridReact
+                       rowData={rowData} // Row Data for Rows
+
+           columnDefs={columnDefs} // Column Defs for Columns
+           defaultColDef={defaultColDef} // Default Column Properties
+
+           animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+           rowSelection='multiple' // Options - allows click selection of rows
+          />
         </centre>
       </Container>
     </div>
